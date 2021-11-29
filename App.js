5971f55cca2e1b7a86da6cfb680c3053a0da58c9
IvanWiesner <ivanwiesner@gmail.com>
2021-11-29 15:38:40 -0700

    styling

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // view = div
+    <View style={{padding: 50}}>
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"}}>
+        <TextInput 
+        placeholder="Course goal" 
+        style={{
+          width: '80%',
+          borderBottomColor: "black",
+          borderWidth: 1,
+          padding: 10}}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
