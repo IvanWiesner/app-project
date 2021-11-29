3d00b3bd17fa9d364023a9a0d086f22fdac0d9e8
IvanWiesner <ivanwiesner@gmail.com>
2021-11-29 16:50:56 -0700

    d

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -28,13 +28,13 @@ export default function App() {
         onPress={addGoalHandler}
         />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map(goal => (
         <View key={goal} style={styles.listItem}>
           <Text>{goal}</Text>
           </View>
         ))}
-      </View> 
+      </ScrollView> 
     </View>
   );
 }
