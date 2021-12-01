6b67e7c4a08102c3cd816834ccc3040fe0bbe23c
IvanWiesner <ivanwiesner@gmail.com>
2021-12-01 14:06:35 -0700

    cleaned up

--- App.js
+++ App.js
@@ -4,15 +4,12 @@ import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 
 // ScrollView - loads everything (slow)
 // FlatList - loads jut whats displayed (fast)
 import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]); 
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText)
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     // previous state into the new state
     // there is no key element so the enteredGoals is jut a string 
     // need to make the key a list or objects
@@ -20,23 +17,13 @@ export default function App() {
     // now this is an array of objects where each object has a key and vlaue property
     setCourseGoals(currentGoals => [
       ...currentGoals, 
-      { id: Math.random().toString(), value: enteredGoal }
+      { id: Math.random().toString(), value: goalTitle }
     ]);
   }
   return (
     // view = div
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-        placeholder="Course goal" 
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-        />
-        <Button title="ADD"
-        onPress={addGoalHandler}
-        />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
       <FlatList 
       keyExtractor={(item, index) => item.id}
       data={courseGoals} 
@@ -49,15 +36,4 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  input: {
-    width: '80%',
-    borderBottomColor: "black",
-    borderWidth: 1,
-    padding: 10
-  }
 });
